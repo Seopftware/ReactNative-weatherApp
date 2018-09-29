d682506c0ccfd80d4ff27748c84a5775b4d00e93
Seopftware <inseop0813@gmail.com>
2018-09-29 10:29:52 +0900

    Weather component css update

--- Weather.js
+++ Weather.js
@@ -1,128 +1,132 @@
-import React, {Component} from "react";
-import {StyleSheet, Text, View} from "react-native";
-import {LinearGradient} from "expo";
-import {Ionicons} from "@expo/vector-icons";
-import ProTypes from "prop-types";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-// export default class App extends Component{
-//     render() {
-//         return(
-//             {/*<LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>*/}
-//                 {/*<View style={styles.upper}>*/}
-//                     {/*<Ionicons color="white" size={144} name="ios-rainy"/>*/}
-//                     {/*<Text style={styles.temp}>Temp here!</Text>*/}
-//                 {/*</View>*/}
-//
-//                 {/*<View style={styles.lower}>*/}
-//                     {/*<Text style={styles.title}>Rainy</Text>*/}
-//                     {/*<Text style={styles.subtitle}>For more info look outside</Text>*/}
-//                 {/*</View>*/}
-//             {/*</LinearGradient>*/}
-//         );
-//     }
-// }
+const weatherCases = {
+  Rain: {
+    colors: ["#00C6FB", "#005BEA"],
+    title: "Raining like a MF",
+    subtitle: "For more info look outside",
+    icon: "weather-rainy"
+  },
+  Clear: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt",
+    icon: "weather-sunny"
+  },
+  Thunderstorm: {
+    colors: ["#00ECBC", "#007ADF"],
+    title: "Thunderstorm in the house",
+    subtitle: "Actually, outside of the house",
+    icon: "weather-lightning"
+  },
+  Clouds: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Clouds",
+    subtitle: "I know, fucking boring",
+    icon: "weather-cloudy"
+  },
+  Snow: {
+    colors: ["#7DE2FC", "#B9B6E5"],
+    title: "Cold as balls",
+    subtitle: "Do you want to build a snowman? Fuck no.",
+    icon: "weather-snowy"
+  },
+  Drizzle: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Drizzle",
+    subtitle: "Is like rain, but gay 🏳️‍🌈",
+    icon: "weather-hail"
+  },
+  Haze: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Haze",
+    subtitle: "Don't know what that is 💩",
+    icon: "weather-hail"
+  },
+  Mist: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Mist!",
+    subtitle: "It's like you have no glasses on.",
+    icon: "weather-fog"
+  }
+};
 
-const WeatherCases ={
-    Rain: {
-        colors:["#00C6FB", "#005BEA"],
-        title: "Raining",
-        subtitle: "For more info look outside",
-        icon: "ios-rainy"
-    },
-    Clear: {
-        colors:["#FEF253", "#FF7300"],
-        title: "Sunny",
-        subtitle: "Go and Walk ",
-        icon: "ios-sunny"
-    },
-    Thunderstorm: {
-        colors:["#00ECBC", "#007ADF"],
-        title: "Thunderstorm",
-        subtitle: "Stay in the house",
-        icon: "ios-thunderstorm"
-    },
-    Clouds: {
-        colors:["#D7D2CC", "#304352"],
-        title: "Clouds",
-        subtitle: "it's weather to read a book",
-        icon: "ios-cloudy"
-    },
-    Snow: {
-        colors:["#7DE2FC", "#B9B6E5"],
-        title: "Snow",
-        subtitle: "Do you want to build a snowman?",
-        icon: "ios-snow"
-    },
-    Drizzle: {
-        colors:["#89F7FE", "#66A6FF"],
-        title: "Drizzle",
-        subtitle: "It's like a rain",
-        icon: "ios-rainy-outline"
-    },
-    Haze: {
-        colors:["#89F7FE", "#66A6FF"],
-        title: "Haze",
-        subtitle: "It's like a cloudy",
-        icon: "ios-rainy-outline"
-    }
+function Weather({ weatherName, temp }) {
+  return (
+    <LinearGradient
+      colors={weatherCases[weatherName].colors}
+      style={styles.container}
+    >
+      <View style={styles.upper}>
+        <MaterialCommunityIcons
+          color="white"
+          size={144}
+          name={weatherCases[weatherName].icon}
+        />
+        <Text style={styles.temp}>{temp}º</Text>
+      </View>
+      <View style={styles.lower}>
+        <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherCases[weatherName].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
 }
 
 Weather.propTypes = {
-    temp:ProTypes.number.isRequired,
-    weatherName: ProTypes.string.isRequired
-
-}
-
-function Weather({temp, weatherName}) {
-    console.log(weatherName)
-    return(
-        <LinearGradient colors={WeatherCases[weatherName].colors} style={styles.container}>
-            <View style={styles.upper}>
-                <Ionicons color="white" size={144} name={WeatherCases[weatherName].icon}/>
-                <Text style={styles.temp}>{temp}℃</Text>
-            </View>
-
-            <View style={styles.lower}>
-                <Text style={styles.title}>{WeatherCases[weatherName].title}</Text>
-                <Text style={styles.subtitle}>{WeatherCases[weatherName].subtitle}</Text>
-            </View>
-        </LinearGradient>
-    )
-}
+  temp: PropTypes.number.isRequired,
+  weatherName: PropTypes.string.isRequired
+};
 
 export default Weather;
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex:1
-    },
-    upper:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center"
-    },
-    lower:{
-        flex:1,
-        alignItems:"flex-start",
-        justifyContent:"flex-end",
-        paddingLeft: 24,
-        marginBottom: 100
-    },
-    temp:{
-        fontSize: 48,
-        color: "white",
-        marginTop: 10
-    },
-    title:{
-        fontSize:38,
-        color: "white",
-        marginBottom: 10,
-        fontWeight:"300"
-    },
-    subtitle:{
-        fontSize:22,
-        color: "white",
-        marginBottom: 24
-    }
+  container: {
+    flex: 1
+  },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "transparent"
+  },
+  temperature: {
+    fontSize: 48,
+    color: "white",
+    justifyContent: "center",
+    backgroundColor: "transparent"
+  },
+  temp: {
+    fontSize: 48,
+    backgroundColor: "transparent",
+    color: "white",
+    marginTop: 10
+  },
+  lower: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    paddingLeft: 25,
+    paddingRight: 25
+  },
+  title: {
+    fontSize: 38,
+    backgroundColor: "transparent",
+    color: "white",
+    marginBottom: 10,
+    fontWeight: "300"
+  },
+  subtitle: {
+    marginBottom: 100,
+    backgroundColor: "transparent",
+    color: "white",
+    fontSize: 24,
+    color: "white",
+    marginBottom: 24
+  }
 });
